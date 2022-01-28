1d618c344e3a0526f9a5f8a76ea40616d909bf29
Matthew <mtthwsprrw@gmail.com>
2022-01-27 19:28:41 -0500

    Update solver.py

--- solver.py
+++ solver.py
@@ -1,183 +1,288 @@
-import re
-import time
-import random
-import psutil
-from pywinauto import Application
+from pywinauto import Desktop
+import win32gui
+import win32con
+import win32api
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import threading
+import psutil
+import tkinter as tk
+import tkinter.ttk as ttk
+import pygubu
+import time
 
-print("Opening Browser")
-driver = webdriver.Chrome()
-print(type(driver))
-#driver.maximize_window()
-driver.get("https://www.wordleunlimited.com/")
-time.sleep(5)
-print("Connecting pywinauto")
-process = psutil.Process(driver.service.process.pid)
+PROJECT_UI = "ui-file.ui"
+WORDLE_URL = "https://www.wordleunlimited.com/"
 
-app = Application()
-app.connect(process=process.children()[0].pid, timeout=10)
-form = app.top_window()
-print("Connected")
-time.sleep(0.5)
+key_map = {
+    "a": 0x41,
+    "b": 0x42,
+    "c": 0x43,
+    "d": 0x44,
+    "e": 0x45,
+    "f": 0x46,
+    "g": 0x47,
+    "h": 0x48,
+    "i": 0x49,
+    "j": 0x4A,
+    "k": 0x4B,
+    "l": 0x4C,
+    "m": 0x4D,
+    "n": 0x4E,
+    "o": 0x4F,
+    "p": 0x50,
+    "q": 0x51,
+    "r": 0x52,
+    "s": 0x53,
+    "t": 0x54,
+    "u": 0x55,
+    "v": 0x56,
+    "w": 0x57,
+    "x": 0x58,
+    "y": 0x59,
+    "z": 0x5A,
+    "enter": win32con.VK_RETURN,
+    "backspace": win32con.VK_BACK
+}
 
-wins = 0
-losses = 0
-streak = 0
+def log(log_message: str, log_type: str):
+    """System logging controller
 
-def calculate():
-    tmp_active_set = active_words.copy()
+    log_message -> str -- log message
+    log_type -> str -- message logging level (i.e. error, warning, log)
+    """
+    if True:
+        print(f"{log_type.upper()}: {log_message}")
 
-    for word in tmp_active_set:
-        # removes words that have incorrect letters in them
-        for letter in incorrect:
-            if letter in word:
-                try:
-                    active_words.remove(word)
-                except:
-                    pass
-                break
+class Dictionary:
+    """
+    """
 
-        # removes words that don't have correct letters in them
-        for letter1 in correct:
-            if letter1 not in word:
-                try:
-                    active_words.remove(word)
-                except:
-                    pass
-                break
-        
-        # removes words that don't have correct letter position in them
-        for check in correct_pos:
-            if check[0] not in word[check[1]]:
-                try:
-                    active_words.remove(word)
-                except:
-                    pass
-                break
+    def __init__(self):
+        """"""
 
-        # removes words that have incorrect letter positioning in them
-        for check1 in incorrect_pos:
-            if check1[0] == word[check1[1]]:
-                try:
-                    active_words.remove(word)
-                except:
-                    pass
-                break
+    def next(self):
+        pass
 
-while True:
-    alphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'}
-    used = set()
-    unused = alphabet
-    correct = set()
-    incorrect = set()
-    correct_pos = set()
-    incorrect_pos = set()
-    active_words = set()
+class Solver:
+    """Represents a Wordle Solver session
 
-    # put all dictionary words into set
-    word_file = open('words_5.txt', 'r')
-    word_lines = word_file.readlines()
-    for word in word_lines:
-        active_words.add(word.strip().lower())
-    word_file.close()
-    words_set = active_words.copy()
+    Methods
+    -------
+        Browser
+        -------
+        open_browser(self, browser: str, url: str)
+            Starts browser session
+        def close_browser(self)
+            Closes browser session
+        def get_browser_pid(self) -> int
+            Gets browser sessions process id
+        def set_browser_pid(self, pid: int)
+            Sets browser session process id
+        def get_browser_driver(self) -> webdriver.chrome.webdriver.WebDriver
+            Gets browser driver
+        def set_browser_driver(self, driver: webdriver.chrome.webdriver.WebDriver)
+            Sets browser driver   
+    """
 
-    letter_pos = 0
+    def __init__(self):
+        """"""
+        self.driver = None
+        self.process_id = None
+        self.paused = None
+        self.run = None
+        self.hwndMain = None
+        self.hwndChild = None
 
-    count = 0
+    def open_browser(self, browser: str, url: str):
+        """Starts browser session
 
-    while True:
-        if count == 0:
-            word = "vitae"
-        elif count == 1:
-            word = "hymns"
+        browser -> (str) -- specify browser type (supported: chrome, firefox)
+        url -> (str) -- url of webpage
+        """
+        if browser == "chrome":
+            self.driver = webdriver.Chrome()
+            self.driver.get(url)
+            self.hwndMain = win32gui.FindWindow("Chrome_WidgetWin_1", "Wordle Unlimited - Play Wordle with Unlimited Words - Google Chrome")
+            self.hwndChild = win32gui.GetWindow(self.hwndMain, win32con.GW_CHILD)
+        elif browser == "firefox":
+            self.driver = webdriver.Firefox()
         else:
-            try:
-                word = random.choice(tuple(active_words))
-            except:
-                word = "based"
+            pass
 
-        #word = str(input("Word: ")).strip().lower()
-        if len(word) == 5 and word in words_set:
-            count += 1
-            form.type_keys(word)
-            form.type_keys('{ENTER}')
-            time.sleep(0.5)
+        self.process_id = psutil.Process(self.driver.service.process.pid).children()[0].pid
+        log(f"{self.process_id} {browser} browser started successfully", "log")
+        self.set_browser_driver(self.driver)
 
-            pageSource = driver.find_element(By.CLASS_NAME, "Game").get_attribute("outerHTML")
+    def close_browser(self):
+        """Closes browser session
+        """
+        self.driver.quit()
+        log(f"{self.process_id} browser driver closed", "log")
+        self.process_id = None
 
-            time.sleep(0.5)
+    def get_browser_pid(self) -> int:
+        """Gets browser sessions process id
 
-            if "Winner!" in re.findall('<p id=\"hint(.*?)</p>', pageSource)[0]:
-                wins += 1
-                streak += 1
-                print(f"\nWins: {wins}\nLosses: {losses}\nStreak: {streak}")
-                time.sleep(5)
-                form.type_keys('{ENTER}')
-                break
-            elif "You lost!" in re.findall('<p id=\"hint(.*?)</p>', pageSource)[0]:
-                losses += 1
-                streak = 0
-                print(f"\nWins: {wins}\nLosses: {losses}\nStreak: {streak}")
-                time.sleep(5)
-                form.type_keys('{ENTER}')
-                break
-            elif "Not a valid word" in re.findall('<p id=\"hint(.*?)</p>', pageSource)[0]:
-                form.type_keys('{BACKSPACE}')
-                form.type_keys('{BACKSPACE}')
-                form.type_keys('{BACKSPACE}')
-                form.type_keys('{BACKSPACE}')
-                form.type_keys('{BACKSPACE}')
-                active_words.remove(word)
-                continue
+        return -> (int) -- process id
+        """
+        return self.process_id
 
-            occurances = re.findall('<div class=\"Row-letter(.*?)</div>', pageSource)
-            for occur in range(5):
-                if "letter-elsewhere" in occurances[occur + letter_pos]:
-                    incorrect_pos.add((word[occur], occur))
-                    correct.add(word[occur])
-                elif "letter-correct" in occurances[occur + letter_pos]:
-                    correct_pos.add((word[occur], occur))
-                    correct.add(word[occur])
-                elif "letter-absent" in occurances[occur + letter_pos]:
-                    if word.count(word[occur]) == 1:
-                        incorrect.add(word[occur])
+    def set_browser_pid(self, pid: int):
+        """Sets browser session process id
 
-            for letter in word:
-                if letter not in used:
-                    used.add(letter)
-                    unused.remove(letter)
+        pid -> (int) -- browser process id        
+        """
+        self.process_id = pid
 
-            letter_pos += 5
-            
-            calculate()
+    def get_browser_driver(self) -> webdriver.chrome.webdriver.WebDriver:
+        """Gets browser driver
 
-            print(f'Used: {used}')
-            print(f'Unused: {unused}')
-            print(f'Correct: {correct}')
-            print(f'Incorrect: {incorrect}')    
-            print(f'Correct Pos: {correct_pos}')
-            print(f'Incorrect Pos: {incorrect_pos}')    
+        return -> (webdriver.chrome.webdriver.WebDriver) -- browser session driver
+        """
+        return self.driver
 
-            if len(active_words) < 100:
-                print(active_words)
-            else:
-                print(len(active_words))
+    def set_browser_driver(self, driver: webdriver.chrome.webdriver.WebDriver):
+        """Sets browser driver
+        
+        driver -> (webdriver.chrome.webdriver.WebDriver) -- browser session driver
+        """
+        self.driver = driver
 
-driver.quit()
+    def start_solver(self, mode: str):
+        """Spawns threading to run solver
 
-"""
-# code used for generating dictionary
+        mode -> (str) -- Auto/Manual solver mode
+        """
+        if mode == "auto":
+            self.run = True
+            self.paused = False
+            threading.Thread(target=self._auto_solver).start()
+            log("Auto solver started", "log")
+        elif mode == "manual":
+            self.run = True
+            self.paused = False
+            threading.Thread(target=self._manual_solver).start()
+            log("Manual solver started", "log")
+        else:
+            log("Invalid solver type", "error")
 
-words = open('words.txt', 'r')
-lines = words.readlines()
-words.close()
+    def pause_solver(self):
+        """Pause current running solver
+        """
+        self.paused = not(self.paused)
+        log("Solver Paused", "log")
 
-for word in lines:
-    if len(word.strip()) == 5:
-        with open('words_5.txt', 'a') as word_file:
-            word_file.write(f'{word.strip().lower()}\n')
-        word_file.close()
-"""
+    def stop_solver(self):
+        """
+        """
+        self.run = False
+        self.paused = True
+        log("Solver Stopped", "log")
 
+    def word_list(self, word: str, enter=False) -> list:
+        """
+        """
+        temp_list = []
+        for letter in word:
+            temp_list.append(letter)
+
+        if enter:
+            temp_list.append("enter")
+
+        return temp_list
+
+    def typing(self, keys: list):
+        """
+        """
+        print("typing")
+        win32gui.SetForegroundWindow(self.hwndMain)
+        for stroke in keys:
+            win32api.SendMessage(self.hwndChild, win32con.WM_KEYDOWN, key_map[f"{stroke}"], 0)
+
+    def _auto_solver(self):
+        """
+        """
+        while self.run:
+            self.typing(self.word_list("raise", True))
+            print(f"Run: {self.run} Paused: {self.paused}")
+            time.sleep(2)
+
+    def _manual_solver(self):
+        """
+        """
+        correction_factor = 0
+        while self.run:
+            time.sleep(2)
+            correction_factor += 2
+            while not(self.paused):
+                print("manual")
+                time.sleep(2 + correction_factor)
+                correction_factor = 0
+
+
+class SolverGUI:
+    """Represents a Wordle Solver GUI session
+
+    Methods
+    -------
+    """
+
+    def __init__(self, solver: Solver, master = None):
+        """
+        
+        """
+        self.builder = builder = pygubu.Builder()
+        builder.add_from_file(PROJECT_UI)
+        self.mainwindow = builder.get_object("mainwindow", master)
+        
+        self.progress_var = None
+        self.speed_var = None
+        builder.import_variables(self, ["progress_var", "speed_var"])
+        
+        builder.connect_callbacks(self)
+
+        self.solver = solver
+    
+    def run(self):
+        self.mainwindow.mainloop()
+
+    def on_open_button(self):
+        """
+        
+        """
+        threading.Thread(target=self.solver.open_browser, args=("chrome", WORDLE_URL)).start()
+        #self.progress_var.set(self.progress_var.get() + 1)       
+
+    def on_start_button(self):
+        """
+        
+        """
+        threading.Thread(target=self.solver.start_solver("auto")).start()
+        self.progress_var.set(self.progress_var.get() + 1)
+
+    def on_pause_button(self):
+        """
+        
+        """
+        threading.Thread(target=self.solver.pause_solver).start()
+
+    def on_stop_button(self):
+        """
+        
+        """
+        threading.Thread(target=self.solver.stop_solver).start()
+
+    def on_close_button(self):
+        """
+        
+        """
+        threading.Thread(target=self.solver.close_browser).start()
+        self.progress_var.set(0)
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    solver = Solver()
+    app = SolverGUI(solver, root)
+    app.run()
+    
